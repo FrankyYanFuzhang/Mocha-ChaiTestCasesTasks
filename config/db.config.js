20460881b02a7ec6632f068a3b02edc470cdf18f
FrankyYanFuzhang <fuzhangyan@astri.org>
2022-10-05 18:32:05 +0800

    add postman export

--- config/db.config.js
+++ config/db.config.js
@@ -5,7 +5,7 @@ const dbConn = mysql.createConnection({
 	host: 'localhost',
 	port: '3306',
 	user: 'root',
-	password: 'YourPass',
+	password: 'Frank2022',
 	database: 'hkscp_db'
 });
 dbConn.connect(function (err) {
